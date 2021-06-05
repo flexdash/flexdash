2087603fc892215e011026d6445c54527891ecf7
Thorsten von Eicken <tve@voneicken.com>
2021-06-05 12:40:50 -0700

    add masory layout components

---
+++ src/components/masonry-brick.vue
@@ -0,0 +1,28 @@
+<!-- MasonryBrick - A brick in a masonry layout that automatically adjusts its row-span so its
+     content fits
+-->
+
+<template>
+  <div class="masonry-brick" :style="style">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.masonry-brick > * { overflow: hidden; }
+</style>
+
+<script scoped>
+export default {
+  name: "MasonryBrick",
+
+  props: {
+    cols: { default: 1 },
+  },
+
+  computed: {
+    style() { return { "grid-column-end": `span ${this.cols}` } },
+  },
+
+}
+</script>
---
+++ src/components/masonry.vue
@@ -0,0 +1,71 @@
+<!-- Masonry - Simple masonry layout, adapted from https://w3bits.com/css-grid-masonry/
+     Copyright ©2021 Thorsten von Eicken, MIT license, see LICENSE file
+-->
+
+<template>
+  <div class="masonry">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.masonry {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20em, 1fr));
+  grid-auto-rows: 16px;
+  grid-gap: 2em;
+}
+</style>
+
+<script scoped>
+export default {
+  name: "Masonry",
+
+  mounted() {
+    console.log("Masonry mounted")
+    this.resizeBricks()
+    const ro = new ResizeObserver(()=> {
+      //console.log("Masonry resized")
+      this.resizeBricks()
+    }).observe(this.$el)
+    if (ro) this.ro = ()=> ro.unobserve(this.$el)
+  },
+  beforeDestroy() {
+    if (this.ro) this.ro()
+    this.ro = undefined
+  },
+
+  methods: {
+    // Resize a masonry brick from https://w3bits.com/css-grid-masonry/
+    resizeBrick(brick, rowHeight, rowGap) {
+      // Spanning for any brick = S
+      // Grid's row-gap = G
+      // Size of grid's implicitly create row-track = R
+      // Height of item content = H
+      // Net height of the item = H1 = H + G
+      // Net height of the implicit row-track = T = G + R
+      // S = H1 / T
+      const inner = brick.children[0]
+      //const H = inner.getBoundingClientRect().height
+      const H = inner.clientHeight
+      const rowSpan = Math.ceil((H+rowGap)/(rowHeight+rowGap))
+      const span = 'span '+rowSpan
+      console.log(`Masonry: H=${Math.round(H)} R=${rowHeight} G=${rowGap}`,
+        `span=${brick.style.gridRowEnd} -> ${rowSpan}`)
+
+      if (rowSpan > 0 && brick.style.gridRowEnd != span)
+        brick.style.gridRowEnd = 'span '+rowSpan
+    },
+
+    resizeBricks() {
+      const grid_style = window.getComputedStyle(this.$el)
+      const rowGap = parseInt(grid_style.getPropertyValue('grid-row-gap'))
+      const rowHeight = parseInt(grid_style.getPropertyValue('grid-auto-rows'))
+      for (let brick of this.$children) {
+        this.resizeBrick(brick.$el, rowHeight, rowGap)
+      }
+    }
+
+  },
+}
+</script>
