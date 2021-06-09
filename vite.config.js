66ddca22135475d425fd61ffade7542a9d1cc4fa
Thorsten von Eicken <tve@voneicken.com>
2021-06-08 22:27:43 -0700

    switch base path to ./

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { createVuePlugin } from 'vite-plugin-vue2'
 import visualizer from 'rollup-plugin-visualizer'
 
 export default {
-  base: '/flexdash/',
+  base: './',
   plugins: [
     createVuePlugin(),
     ViteComponents({
