db70a656de734c99c60fafa1a61911bb617576e6
Thorsten von Eicken <tve@voneicken.com>
2021-06-16 16:59:37 -0700

    switch color-picker to use v-overlay

--- src/components/color-picker.vue
+++ src/components/color-picker.vue
@@ -13,34 +13,35 @@
         </v-btn>
       </template>
     </v-text-field>
-    <v-card v-if="show_picker" class="overlay d-flex flex-column">
-      <v-card-title class="d-flex align-baseline width100 pt-2 pb-0">
-        <span>{{label}}</span>
-        <v-spacer></v-spacer>
-        <v-btn elevation=0 icon @click="show_picker=false">
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-      </v-card-title>
-      <v-color-picker hide-canvas hide-sliders mode="hexa" show-swatches swatches-max-height="auto"
-                      :swatches="swatches" :value="value" @update:color="changeColor">
-      </v-color-picker>
-    </v-card>
+    <v-overlay v-model="show_picker" class="color-picker" :dark="false">
+      <v-card class="d-flex flex-column">
+        <v-card-title class="d-flex align-baseline width100 pt-0 pb-0">
+          <span>{{label}}</span>
+          <v-spacer></v-spacer>
+          <v-btn elevation=0 icon @click="show_picker=false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-color-picker hide-canvas hide-sliders mode="hexa" show-swatches swatches-max-height="auto"
+                        width="100%"
+                        :swatches="swatches" :value="value" @update:color="changeColor">
+        </v-color-picker>
+      </v-card>
+    </v-overlay>
   </div>
 </template>
 
-<style scoped>
-.overlay {
-  position: absolute; top: 8px; z-index: 12;
-  width: 300px; height: 95%; 
-}
-</style>
-
 <style>
-.v-color-picker { min-height: 0px; display: flex; flex-direction: column; }
-.v-color-picker__edit { margin-top: 0px; }
-.v-color-picker__controls { padding-bottom: 0px; }
-.v-color-picker__input > input { margin-bottom: 0px; }
-.v-color-picker__swatches { min-height: 0px; max-height: 80% !important; }
+.color-picker .v-overlay__content { height: 95%; width: 95%; max-width: 500px;  }
+.color-picker .v-card { height: 100%; width: 100%; }
+.color-picker .v-color-picker {
+  flex-grow: 1;  min-height: 0px;
+  display: flex; flex-direction: column;
+}
+.color-picker .v-color-picker__edit { margin-top: 0px; }
+.color-picker .v-color-picker__controls { padding-bottom: 0px; }
+.color-picker .v-color-picker__input > input { margin-bottom: 0px; }
+.color-picker .v-color-picker__swatches { min-height: 0px; max-height: 80% !important; }
 </style>
 
 <script scoped>
