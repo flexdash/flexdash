55b989c4d1dba707c8b6433932f92cf7717d675f
Thorsten von Eicken <tve@voneicken.com>
2021-12-15 09:37:46 -0800

    EditPlusMinus: fix issues

--- src/components/edit-plus-minus.vue
+++ src/components/edit-plus-minus.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="d-flex justify-center align-center">
-    <v-btn x-small icon elevation=2 @click="dec"><v-icon>mdi-minus</v-icon></v-btn></v-col>
-    <v-chip small class="mx-1">{{fmt()}}</v-chip></v-col>
-    <v-btn x-small icon elevation=2 @click="inc"><v-icon>mdi-plus</v-icon></v-btn></v-col>
+    <v-btn x-small icon elevation=2 @click="dec"><v-icon>mdi-minus</v-icon></v-btn>
+    <v-chip small class="mx-1">{{fmt}}</v-chip>
+    <v-btn x-small icon elevation=2 @click="inc"><v-icon>mdi-plus</v-icon></v-btn>
   </div>
 </template>
 
@@ -13,27 +13,35 @@ export default {
   props: {
     range: Array,
     value: Number,
-    unit: String,
+    unit: { type: String, default: "" },
   },
 
-  data() {
-    //console.log("edit-plus-minus.value:", this.value)
-    let ix = this.range.findIndex(x => x == this.value)
-    if (ix == -1) ix = 0
-    return {
-      ix: ix,
-    };
-  },
+  data() { return {
+    ix: this.findIx(this.value),
+  }},
 
-  methods: {
-    inc() { if (this.ix < this.range.length-1) this.ix += 1; this.emit() },
-    dec() { if (this.ix > 0) this.ix -= 1; this.emit() },
+  computed: {
     fmt() {
       let v = this.range[this.ix]
       if (typeof v == 'number') return "" + v + " " + this.unit
       else return v
     },
+  },
+
+  watch: {
+    value(val) { this.ix = this.findIx(val) },
+  },
+
+  methods: {
+    inc() { if (this.ix < this.range.length-1) this.ix += 1; this.emit() },
+    dec() { if (this.ix > 0) this.ix -= 1; this.emit() },
     emit() { this.$emit('update:value', this.range[this.ix]) },
+    // findIx finds the index at which the passed value is found
+    findIx(v) {
+      //console.log("edit-plus-minus.value:", v)
+      let ix = this.range.findIndex(x => x == v)
+      return ix >= 0 ? ix : 0
+    },
   },
 }
 </script>
