ecb2a17b86e484db8a00ad5a64fad98723efc013
Thorsten von Eicken <tve@voneicken.com>
2021-12-31 12:52:51 -0800

    make text view editable

--- src/widgets/text-view.vue
+++ src/widgets/text-view.vue
@@ -2,15 +2,43 @@
      Copyright ©2021 Thorsten von Eicken, MIT license, see LICENSE file
 -->
 <template>
-  <div style="width:100%; height:100%;overflow-y:scroll;">
-    <v-card-text class="pt-1">
-      <pre>{{text}}</pre>
+  <div class="mt-0 width100 d-flex flex-column" style="height: 100%;">
+    <!-- title and edit button -->
+    <v-card-text class="d-flex pa-0 pt-1 mb-0">
+      <span v-if="title" class="ml-auto mr-1 text-no-wrap">{{title}}</span>
+      <!-- edit button (when not editing) -->
+      <v-btn small icon class="title-btn mr-auto ml-0" v-if="editable && !editing" @click="handleEdit">
+        <v-icon small>mdi-pencil</v-icon>
+      </v-btn>
+      <!-- save/cancel buttons (when editing) -->
+      <v-btn small icon class="title-btn mr-1 ml-0" v-if="editing" @click="handleCancel">
+        <v-icon small>mdi-close-thick</v-icon>
+      </v-btn>
+      <v-btn small icon class="title-btn mr-auto ml-1" v-if="editing" @click="handleSave">
+        <v-chip x-small pill color="green">save</v-chip>
+      </v-btn>
+      <span class="mr-auto"></span>
     </v-card-text>
+
+    <!--div style="width:100%; height:100%;"-->
+      <!-- not editing... -->
+      <v-card-text v-if="!editing" class="pt-1 flex-grow-1 flex-shrink-1 overflow-auto">
+        <pre>{{text}}</pre>
+      </v-card-text>
+      <!-- editing... -->
+      <v-textarea dense solo full-width flat hide-details v-if="editing" :value=text></v-textarea>
+    <!--/div-->
   </div>
 </template>
 
 <style scoped>
-pre { font-size: 85%; line-height: 125%; }
+  pre { font-size: 85%; line-height: 125%; }
+  .title-btn { position: relative; top: -4px; }
+  .theme--light .v-btn--icon { background-color: rgba(255, 255, 255, 0.6); }
+  .theme--dark .v-btn--icon  { background-color: rgba(30, 30, 30, 0.6); }
+  .theme--light .v-card__text { color: rgba(0, 0, 0, 0.6); }
+  .theme--dark .v-card__text  { color: rgba(255, 255, 255, 0.7); }
+  textareaxx { width:100%; height:100%; max-width:100%; cursor:text; overflow:auto; }
 </style>
 
 <script scoped>
@@ -23,8 +51,24 @@ export default {
 `,
 
   props: {
+    title: { type: String, default: 'TextView' },
     text: { type: String, default: "Hi there!" },
     autoscroll: { type: Boolean, default: false, tip: "Auto-scroll to bottom" },
+    editable: { type: Boolean, default: false, tip: "allow editing of the text"},
+  },
+
+  data() { return {
+    editing: false,
+  }},
+
+  methods: {
+    handleEdit() { this.editing = true },
+    handleCancel() { this.editing = false },
+    handleSave() {
+      this.editing = false;
+      this.$emit('send', this.new_values)
+      this.new_values = {}
+    },
   },
 
 }
