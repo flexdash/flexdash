3eb50d477a13b613ab68d70edb2b8227bf7948b5
Thorsten von Eicken <tve@voneicken.com>
2021-12-31 12:50:39 -0800

    allow edit mode to be specified in flexdash options

--- src/main.js
+++ src/main.js
@@ -78,7 +78,7 @@ const app = new Vue({
 
   data: { // https://stackoverflow.com/questions/51275301/how-to-react-to-a-global-variable-with-vue
     // editMode encodes whether the edit toggle is on/off, access anywhere as this.$root.editMode
-    editMode: false,
+    editMode: fo.edit,
     // current route, gets initialized with the initial location that loads FlexDash
     route: window.location.hash,
     params: (new URL(document.location)).searchParams,
