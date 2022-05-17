29b8fe5a0183146bc6d833eaa76452b7fe4b4780
Thorsten von Eicken <tve@voneicken.com>
2022-05-16 17:56:08 -0700

    support array of arrays and map of maps in simple-table

--- src/widgets/simple-table.vue
+++ src/widgets/simple-table.vue
@@ -2,18 +2,16 @@
      Copyright ©2021 Thorsten von Eicken, MIT license, see LICENSE file
 -->
 <template>
-  <v-simple-table dense fixed-header class="simple-table mt-2">
-    <template v-slot:default>
-      <thead><tr>
-        <th class="px-2" v-for="col in col_labels" :key="col">{{col}}</th>
-      </tr></thead>
-      <tbody>
-        <tr v-for="k in row_keys" :key="k">
-          <td class="px-2" v-for="col in col_keys" :key="col">{{data_at(k, col)}}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <v-table fixed-header class="simple-table mt-2">
+    <thead><tr>
+      <th class="px-2" v-for="col in col_labels" :key="col">{{col}}</th>
+    </tr></thead>
+    <tbody>
+      <tr v-for="k in row_keys" :key="k">
+        <td class="px-2" v-for="col in col_keys" :key="col">{{data_at(k, col)}}</td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
 
 <style scoped>
@@ -30,11 +28,20 @@ export default {
   // help displayed in the UI: the first line is used in the widgets menu and is always shown in
   // the edit card. Successive lines can be expanded in the card and are markdown-formatted.
   help: `Display data in simple tabular form.
-Yadda yadda.`,
+The data can consist of an array of rows, or a map of row-key to row.
+In the case of an array, the rows are displayed in array order, in the case of a map,
+the rows are displayed in sorted-key order, the keys themselves are not displayed.
+
+Each row must be an array of values or a map of column-key to value.
+In the case of a map, the order of the columns is determined by the columns prop and
+the key is not displayed, in the case of an array, the columns prop is ignored.
+
+The labels prop determines the labels at the top of the columns.
+`,
 
   props: {
     data: { default: null, tip: "array of data rows, or map of rows"},
-    columns: { type: Array, default: null, tip: "per column key to index data"},
+    columns: { type: Array, default: null, tip: "per column key to index into row map"},
     labels: { type: Array, default: null, tip: "array of column labels"},
   },
 
@@ -47,7 +54,9 @@ Yadda yadda.`,
     col_keys() { 
       if (this.columns) return this.columns
       if (!this.data) return []
-      return Object.keys(this.data[this.keys[0]]).sort()
+      let first_row = Array.isArray(this.data) ? this.data[0] : this.data[this.row_keys[0]]
+      if (Array.isArray(first_row)) return first_row.length
+      else return Object.keys(first_row).sort()
     },
     col_labels() { 
       if (this.labels) return this.labels
@@ -57,10 +66,12 @@ Yadda yadda.`,
 
   methods: {
     data_at(row, col) {
-      if (Array.isArray(this.data)) return this.data[row-1][col]
-      return this.data[row] ? this.data[row][col] : null },
+      let r = Array.isArray(this.data) ? this.data[row-1] : this.data[row]
+      if (Array.isArray(r)) return r[col-1]
+      else if (typeof r == 'object') return r[col]
+      else return null
+    },
   },
 
 }
 </script>
-
