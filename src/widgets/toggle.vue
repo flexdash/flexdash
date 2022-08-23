09a66b17b4dac533c966a140fecd898a607e7d9c
Thorsten von Eicken <tve@voneicken.com>
2022-08-22 21:06:12 -0700

    toggle: add loopback option, improve description

--- src/widgets/toggle.vue
+++ src/widgets/toggle.vue
@@ -23,7 +23,16 @@ export default {
 
   help: `Simple on/off toggle switch.
 Sends a pre-determined value when toggled on or off. The current state can be set via the
-value input: the toggle will be "on" if the value is equal to the "on_value" input.`,
+value input: the toggle will be "on" IF the value is equal to the "on_value" input.
+
+By default clicking the toggle shows a "ripple" effect and outputs a message but does not toggle
+the switch. The switch is only toggled in response to an input change. If no input is received,
+the toggle will "fall back" to its prior state and repeated clicking will send the same output.
+
+By setting \`loopback\` to true, the toggle will internally feed the output value to its input
+causing the toggle to immediately change to the new state. Repeated clicking will then alternate
+between the on & off values.
+`,
 
   props: {
     value: { default: null, dynamic: "toggle", tip: "set toggle value" },
@@ -31,7 +40,8 @@ value input: the toggle will be "on" if the value is equal to the "on_value" inp
     color: { type: String, default: "primary" },
     on_value: { default: true, tip: "value sent when switched on" },
     off_value: { default: false, tip: "value sent when switched off" },
-    show_value: { type: Boolean, default: true, tip: "show current value" },
+    show_value: { type: Boolean, default: true, tip: "display current value" },
+    loopback: { type: Boolean, default: false, tip: "internally loop-back output to input" },
   },
 
   output: { default: null },
@@ -61,13 +71,18 @@ value input: the toggle will be "on" if the value is equal to the "on_value" inp
   },
 
   watch: {
-    value: { immediate: true, handler(v) { this.val = v } },
+    value: { immediate: true, handler(v) { this.val = v; console.log("toggle watch:", v) } },
   },
 
   methods: {
     // change from v-switch is only emitted when the user toggles, not when input value changes
     change(ev) {
       this.$emit('send', ev)
+      const curVal = this.val
+      console.log("toggle emits", ev, "was", curVal)
+      if (this.loopback) this.val = ev
+      // if (!this.loopback) setTimeout(() => {this.val = curVal; console.log("set toggle to", curVal)}, 50)
+      // this.val = ev
     },
   },
 
