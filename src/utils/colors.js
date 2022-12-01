29834476083992d85831dccdadc14cd71fe33b08
Thorsten von Eicken <tve@voneicken.com>
2022-11-30 23:27:07 -0800

    colors: reduce logging

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -15,17 +15,17 @@ export function color2hhex(color, theme_colors) {
 
   // handle material design palette colors
   let cc = color.split('-')
-  console.log(`cc0: ${JSON.stringify(cc)}`)
+  //console.log(`cc0: ${JSON.stringify(cc)}`)
   if (cc.length > 4) return colorNotFound
   if (cc.length == 4 || cc.length == 2) {
     // first two are color name, e.g. deep-purple or deep-purple-lighten-3
     let cap = cc[1].charAt(0).toUpperCase() + cc[1].slice(1)
     cc.splice(0, 2, cc[0] + cap) // deepPurple
   }
-  console.log(`cc1: ${JSON.stringify(cc)}`)
+  //console.log(`cc1: ${JSON.stringify(cc)}`)
   if (cc[0] in colors.shades) return colors.shades[cc[0]] // black, white, transparent
   if (!(cc[0] in colors)) return colorNotFound
   const shade = cc.length > 1 ? cc[1]+cc[2] : 'base'
-  console.log(`shade: ${shade} -> ${colors[cc[0]][shade]}`)
+  //console.log(`shade: ${shade} -> ${colors[cc[0]][shade]}`)
   return colors[cc[0]][shade] || colorNotFound
 }
